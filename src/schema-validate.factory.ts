b806be7a5e64b54bfb0aeefde39314cbdce07345
mechtelm <mechtel@iqb.hu-berlin.de>
2023-10-19 18:38:03 +0200

    Add profile config

--- src/schema-validate.factory.ts
+++ src/schema-validate.factory.ts
@@ -100,9 +100,73 @@ export abstract class SchemaValidateFactory {
                 }
             }
         } else {
-            console.log(`\x1b[0;31mERROR\x1b[0m config file '${sourceFilename}' not found`);
+            console.log(`\x1b[0;31mERROR\x1b[0m profile file '${sourceFilename}' not found`);
             process.exitCode = 1;
         }
         return mdProfile;
+    }
+
+    public static validateConfig(sourceFilename: string, schemaFilename: string): ConfigData | null {
+        let schema;
+        let mdConfig: ConfigData | null = null;
+        const fs = require('fs');
+        if (fs.existsSync(schemaFilename)) {
+            try {
+                schema = fs.readFileSync(schemaFilename, 'utf8');
+            } catch (err) {
+                console.log(`\x1b[0;31mERROR\x1b[0m reading schema '${schemaFilename}':`);
+                console.error(err);
+                process.exitCode = 1;
+                schema = null;
+            }
+            if (schema) {
+                let compiledSchema;
+                const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+                try {
+                    compiledSchema = ajv.compile(JSON.parse(schema))
+                } catch (err) {
+                    console.log(`\x1b[0;31mERROR\x1b[0m parsing schema '${schemaFilename}':`);
+                    console.error(err);
+                    process.exitCode = 1;
+                    compiledSchema = null;
+                }
+                if (compiledSchema) {
+                    if (fs.existsSync(sourceFilename)) {
+                        try {
+                            const config_data_raw = fs.readFileSync(sourceFilename, 'utf8');
+                            mdConfig = JSON.parse(config_data_raw);
+                        } catch (err) {
+                            console.log(`\x1b[0;31mERROR\x1b[0m reading and parsing config file '${sourceFilename}':`);
+                            console.error(err);
+                            mdConfig = null;
+                            process.exitCode = 1;
+                        }
+                        if (mdConfig) {
+                            try {
+                                const valid = compiledSchema ? compiledSchema(mdConfig) : null;
+                                if (!valid) {
+                                    console.log(`\x1b[0;31mERROR\x1b[0m invalid config file '${sourceFilename}':`);
+                                    console.error(compiledSchema ? compiledSchema.errors : 'error unknown')
+                                    mdConfig = null;
+                                    process.exitCode = 1;
+                                }
+                            } catch (err) {
+                                console.log(`\x1b[0;31mERROR\x1b[0m invalid config file '${sourceFilename}':`);
+                                console.error(err);
+                                mdConfig = null;
+                                process.exitCode = 1;
+                            }
+                        }
+                    } else {
+                        console.log(`\x1b[0;31mERROR\x1b[0m config file '${sourceFilename}' not found`);
+                        process.exitCode = 1;
+                    }
+                }
+            }
+        } else {
+            console.log(`\x1b[0;31mERROR\x1b[0m config file '${sourceFilename}' not found`);
+            process.exitCode = 1;
+        }
+        return mdConfig;
     }
 }
--- src/validate-md-profile.ts
+++ src/validate-md-profile.ts
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 import {SchemaValidateFactory} from "./schema-validate.factory";
 
-const fs = require('fs');
-const schemaFilename = 'md-profile.schema.json';
-let fileToCheckName = './profile.json';
+let configFileName = './profile-config.json';
 if (process.argv[2]) {
-    fileToCheckName = `./${process.argv[2]}`;
-}
-let schemaFullFilename = `${__dirname}/${schemaFilename}`;
-if (!fs.existsSync(schemaFullFilename)) {
-    schemaFullFilename = `./json_schema/md-profile/${schemaFilename}`;
+    configFileName = `./${process.argv[2]}`;
 }
 
-const myMDProfile = SchemaValidateFactory.validateProfile(fileToCheckName, schemaFullFilename);
-if (myMDProfile) console.log(`${myMDProfile.groups.length} groups found in '${fileToCheckName}'.`);
+const fs = require('fs');
+const configSchemaFilename = 'md-profile-config.schema.json';
+let configSchemaFullFilename = `${__dirname}/${configSchemaFilename}`;
+if (!fs.existsSync(configSchemaFullFilename)) {
+    configSchemaFullFilename = `./json_schema/md-profile-config/${configSchemaFilename}`;
+}
+
+const mdConfig = SchemaValidateFactory.validateConfig(configFileName, configSchemaFullFilename);
+if (mdConfig) {
+    console.log(`config file '${configFileName}' is valid.`);
+    const profileSchemaFilename = 'md-profile.schema.json';
+    let profileSchemaFullFilename = `${__dirname}/${profileSchemaFilename}`;
+    if (!fs.existsSync(profileSchemaFullFilename)) {
+        profileSchemaFullFilename = `./json_schema/md-profile/${profileSchemaFilename}`;
+    }
+    mdConfig.profiles.forEach(p => {
+        const myMDProfile = SchemaValidateFactory.validateProfile(p, profileSchemaFullFilename);
+        if (myMDProfile) console.log(`${myMDProfile.groups.length} groups found in '${p}'.`);
+    })
+}
+
+
+
